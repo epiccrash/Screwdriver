ce77e6c25de3d28e71510b058eb94381ce0c551e
Ilana Franklin <ilana.franklin@gmail.com>
2019-10-21 21:21:56 -0400

    Some basic bar manager work

---
+++ Assets/Scripts/BarManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Singleton(SingletonAttribute.Type.LoadedFromResources, true, "BarManager")]
+public class BarManager : Singleton<BarManager>
+{
+    private List<CustomerSlot> _customerSlots;
+
+    protected override void Awake()
+    {
+        Initialize();
+    }
+
+    public override void Initialize()
+    {
+        _customerSlots = new List<CustomerSlot>();
+
+        // Find all slots in the scene.
+        GameObject[] slotObjs = GameObject.FindGameObjectsWithTag("CustomerSlot");
+
+        foreach (GameObject slot in slotObjs)
+        {
+            _customerSlots.Add(slot.GetComponent<CustomerSlot>());
+        }
+    }
+
+    public CustomerSlot GetAndLockAvailableSlot()
+    {
+        List<CustomerSlot> availableSeats = new List<CustomerSlot>();
+
+        foreach (CustomerSlot slot in _customerSlots)
+        {
+            if (slot.IsFree)
+            {
+                availableSeats.Add(slot);
+            }
+        }
+
+        if (availableSeats.Count <= 0)
+        {
+            return null;
+        }
+
+        // Get a random seat from the free seats.
+        int randomIndx = UnityEngine.Random.Range(0, availableSeats.Count);
+
+        availableSeats[randomIndx].Lock();
+        return availableSeats[randomIndx];
+    }
+}
---
+++ Assets/Scripts/CustomerSlot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerSlot : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _standLocation;
+
+    private bool _isFree = true;
+
+    public bool IsFree { get { return _isFree; } }
+
+    private void Start()
+    {
+        CustomerSlot slot = BarManager.Instance.GetAndLockAvailableSlot();
+        slot.Lock();
+        Debug.Log(slot);
+    }
+
+    public void Lock()
+    {
+        _isFree = false;
+    }
+}
