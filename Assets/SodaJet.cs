a07a8943b64166c2601cd8456711a97c37a4d4f4
Joey Perrino <epiccrashv3@gmail.com>
2019-10-22 21:08:28 -0500

    Attempting Soda Jet input

--- Assets/SodaJet.cs
+++ Assets/SodaJet.cs
@@ -5,9 +5,15 @@ using Valve.VR;
 
 public class SodaJet : MonoBehaviour
 {
-    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.LeftHand;
+    public SteamVR_Input_Sources inputSource2 = SteamVR_Input_Sources.RightHand;
 
-    [SerializeField] private Vector2 joystick;
+    private Vector2 joystickL;
+    private Vector2 joystickR;
+
+    private GameObject leftHand;
+    private GameObject rightHand;
+
     [SerializeField] private GameObject liquidPrefab;
     [SerializeField] private GameObject liquidPrefab2;
     [SerializeField] private int force = 3000;
@@ -19,12 +25,19 @@ public class SodaJet : MonoBehaviour
     {
         spout = transform.GetChild(0).gameObject;
         StartCoroutine(Squirt());
+
+        leftHand = GameObject.Find("LeftHand");
+        rightHand = GameObject.Find("RightHand");
+        joystickL = SteamVR_Actions._default.Joystick.GetAxis(inputSource);
     }
 
     // Update is called once per frame
     void Update()
     {
-        joystick = SteamVR_Actions.default_Joystick.GetAxis(inputSource);
+        joystickL = SteamVR_Actions._default.Joystick.GetAxis(inputSource);
+        joystickR = SteamVR_Actions._default.Joystick.GetAxis(inputSource2);
+        //joystickL = new Vector2(Input.GetAxis("JoystickLeftH"), Input.GetAxis("JoystickLeftV"));
+        //joystickR = new Vector2(Input.GetAxis("JoystickLeftH"), Input.GetAxis("JoystickLeftV"));
     }
 
     IEnumerator Squirt()
@@ -34,16 +47,23 @@ public class SodaJet : MonoBehaviour
             Vector3 direction = spout.transform.position - transform.position;
             direction.Normalize();
 
-            if (joystick.y >= 0.1f)
+            if (transform.parent != null)
             {
-                GameObject newDrop = Instantiate(liquidPrefab);
-                newDrop.transform.position = spout.transform.position;
-                newDrop.GetComponent<Rigidbody>().AddForce(direction * force);
-            } else if (joystick.y <= -0.1f)
-            {
-                GameObject newDrop = Instantiate(liquidPrefab2);
-                newDrop.transform.position = spout.transform.position;
-                newDrop.GetComponent<Rigidbody>().AddForce(direction * force);
+                print(transform.parent);
+                if ((transform.parent == leftHand && joystickL.y >= 0.1f) ||
+                    (transform.parent == rightHand && joystickR.y >= 0.1f))
+                {
+                    GameObject newDrop = Instantiate(liquidPrefab);
+                    newDrop.transform.position = spout.transform.position;
+                    newDrop.GetComponent<Rigidbody>().AddForce(direction * force);
+                }
+                else if ((transform.parent == leftHand && joystickL.y <= -0.1f) ||
+                         (transform.parent == rightHand && joystickR.y <= -0.1f))
+                {
+                    GameObject newDrop = Instantiate(liquidPrefab2);
+                    newDrop.transform.position = spout.transform.position;
+                    newDrop.GetComponent<Rigidbody>().AddForce(direction * force);
+                }
             }
 
             yield return new WaitForSeconds(0.005f);
