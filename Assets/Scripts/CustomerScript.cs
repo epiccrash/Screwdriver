06ae125b70da27c27adb4a17a91e4de667e89e5f
Ilana Franklin <ilana.franklin@gmail.com>
2019-10-23 20:23:37 -0400

    NPCs wander while they wait for drinks

--- Assets/Scripts/CustomerScript.cs
+++ Assets/Scripts/CustomerScript.cs
@@ -13,6 +13,9 @@ public enum CustomerState
 
 public class CustomerScript : MonoBehaviour
 {
+    public const float DestTolerance = 0.1f;
+    public const float WanderRadius = 3f;
+
     [SerializeField]
     private List<DrinkRecipe> _orderableDrinks;
 
@@ -20,13 +23,27 @@ public class CustomerScript : MonoBehaviour
     private int _drunkThreshhold;
 
     [Header("Drink Order Timing")]
+
     [SerializeField]
     private float _minTimeBetweenDrinks;
 
     [SerializeField]
     private float _maxTimeBetweenDrinks;
 
+    [Header("Wander Wait Timing")]
+
+    [SerializeField]
+    private float _minWanderPosWaitTime;
+
+    [SerializeField]
+    private float _maxWanderPosWaitTime;
+
     private CustomerSlot _currentSlot;
+
+    private Transform _currentWanderLocation;
+    private float _wanderPauseTimer;
+    private bool _isWaitingAtWanderPos;
+
     private int _alchoholLevel;
     private float _timeUntilNextDrink;
     private CustomerState _state;
@@ -37,32 +54,96 @@ public class CustomerScript : MonoBehaviour
         _agent = GetComponent<NavMeshAgent>();
 
         _alchoholLevel = 0;
-        _timeUntilNextDrink = UnityEngine.Random.Range(_minTimeBetweenDrinks, _maxTimeBetweenDrinks);
 
-        Debug.Log(_timeUntilNextDrink);
+        RandomizeDrinkTimer();
+        RandomizeWanderWaitTimer();
+
+        // We'll manually set state this first time, since we don't want to apply special changes.
         _state = CustomerState.Idle;
     }
 
     private void Update()
     {
-        _timeUntilNextDrink -= Time.deltaTime;
+        // Countdown drink timer.
+        if (_state == CustomerState.Idle)
+        {
+            _timeUntilNextDrink -= Time.deltaTime;
 
-        // If it's time to order, get in the queue for a slot.
-        if (_state == CustomerState.Idle && _timeUntilNextDrink <= 0f)
-        {
-            _state = CustomerState.ReadyToOrder;
-            BarManager.Instance.EnterCustomerQueue(this);
+            // If it's time to order, get in the queue for a slot.
+            if (_timeUntilNextDrink <= 0)
+            {
+                _state = CustomerState.ReadyToOrder;
+                BarManager.Instance.EnterCustomerQueue(this);
+            }
         }
-        else if (_state == CustomerState.WalkingToSlot && _agent.remainingDistance <= 0.1f)
+
+        // Randomly wander around the room.
+        if (_state == CustomerState.Idle || _state == CustomerState.ReadyToOrder)
         {
-            _state = CustomerState.WaitingForDrink;
+            // Have we reached our position?
+            // If yes, lets count down our timer
+            // Is our timer below 0?
+            // Lets get a new position and restart the time
+            // Have we reached our random wander position?
+            if (_agent.remainingDistance <= DestTolerance)
+            {
+                _wanderPauseTimer -= Time.deltaTime;
+
+                if (_wanderPauseTimer <= 0)
+                {
+                    RandomizeWanderWaitTimer();
+                    _agent.destination = GetRandomWanderPosition();
+                }
+            }
         }
+
+        // Check if we've arrived at a customer slot yet.
+        if (_state == CustomerState.WalkingToSlot && _agent.remainingDistance <= DestTolerance)
+        {
+            ChangeState(CustomerState.WaitingForDrink);
+        }
+    }
+
+    private void RandomizeDrinkTimer()
+    {
+        _timeUntilNextDrink = UnityEngine.Random.Range(_minTimeBetweenDrinks, _maxTimeBetweenDrinks);
+    }
+
+    private void RandomizeWanderWaitTimer()
+    {
+        _wanderPauseTimer = UnityEngine.Random.Range(_minWanderPosWaitTime, _maxWanderPosWaitTime);
+    }
+
+    private void ChangeState(CustomerState newState)
+    {
+        // If we're not transitioning between wander states, reset the timer.
+        if (_state != CustomerState.Idle || newState != CustomerState.ReadyToOrder)
+        {
+            _wanderPauseTimer = 0;
+        }
+
+        _state = newState;
+    }
+
+    private Vector3 GetRandomWanderPosition()
+    {
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * WanderRadius;
+        randomDirection += transform.position;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randomDirection, out navHit, WanderRadius, -1);
+
+        Vector3 newWanderPos = navHit.position;
+
+        newWanderPos.x = Mathf.Max(newWanderPos.x, -3.5f);
+
+        return newWanderPos;
     }
 
     public void AssignSlot(CustomerSlot slot)
     {
         _currentSlot = slot;
-        _state = CustomerState.WalkingToSlot;
+        ChangeState(CustomerState.WalkingToSlot);
 
         _agent.destination = slot.StandLocation.position;
     }
@@ -71,9 +152,12 @@ public class CustomerScript : MonoBehaviour
     {
         // Test if we got the right drink.
 
-        // Give tips if we want/
+        // Are we drunk?
 
-        _timeUntilNextDrink = UnityEngine.Random.Range(_minTimeBetweenDrinks, _maxTimeBetweenDrinks);
-        _state = CustomerState.Idle;
+        // Give tips if we want.
+
+        // Go back to partying before our next drink.
+        RandomizeDrinkTimer();
+        ChangeState(CustomerState.Idle);
     }
 }
