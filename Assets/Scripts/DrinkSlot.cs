ce96c5caaeced99f27060e8e36a565319b0bd568
Ilana Franklin <ilana.franklin@gmail.com>
2019-10-29 16:33:58 -0400

    Some debug rendering for customer slots

---
+++ Assets/Scripts/DrinkSlot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkSlot : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        // Draw the drink zone collider.
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawCube(transform.position, new Vector3(1, 1, 1));
+    }
+}
