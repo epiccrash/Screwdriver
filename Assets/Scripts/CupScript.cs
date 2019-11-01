4bd26e9a88f79931dc4a7b978d4a3d7eb52b7487
Ilana Franklin <ilana.franklin@gmail.com>
2019-11-01 11:10:20 -0400

    Added a cup script & trigger for detecting ingredients

---
+++ Assets/Scripts/CupScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupScript : MonoBehaviour
+{
+    List<IngredientUnit> _itemsInCup;
+
+    private void Start()
+    {
+        _itemsInCup = new List<IngredientUnit>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
