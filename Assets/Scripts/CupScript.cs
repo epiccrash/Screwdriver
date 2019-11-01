9c57cbf0bdb199f0e47ed12eeea2488f24f5264a
Ilana Franklin <ilana.franklin@gmail.com>
2019-11-01 11:36:56 -0400

    Cup can detect placed ingredients

--- Assets/Scripts/CupScript.cs
+++ Assets/Scripts/CupScript.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class CupScript : MonoBehaviour
 {
-    List<IngredientUnit> _itemsInCup;
+    private IDictionary<IngredientType, int> _itemsInCup;
 
     private void Start()
     {
-        _itemsInCup = new List<IngredientUnit>();
+        _itemsInCup = new Dictionary<IngredientType, int>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("IceCube"))
+        {
+            AddOrIncreaseIngredient(IngredientType.Ice);
+        }
+        else if (other.gameObject.CompareTag("Water"))
+        {
+            IngredientScript ingredient = other.gameObject.GetComponent<IngredientScript>();
+            AddOrIncreaseIngredient(ingredient.IngredientType);
+        }
+    }
 
+    private void AddOrIncreaseIngredient(IngredientType ingredient)
+    {
+        if (_itemsInCup.ContainsKey(ingredient))
+        {
+            _itemsInCup[ingredient]++;
+        }
+        else
+        {
+            _itemsInCup.Add(ingredient, 1);
+        }
+
+        print(ingredient + ": " + _itemsInCup[ingredient]);
     }
 }
