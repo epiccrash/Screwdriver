117d1314df500a0abb8389a63c2b31bf2d7e253b
Ilana Franklin <ilana.franklin@gmail.com>
2019-11-01 11:37:19 -0400

    Added ingredient script

---
+++ Assets/Scripts/IngredientScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientScript : MonoBehaviour
+{
+    [SerializeField]
+    private IngredientType _ingredientType;
+
+    public IngredientType IngredientType { get { return _ingredientType; } }
+
+}
