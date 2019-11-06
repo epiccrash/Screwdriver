01bcf638af479f3b67e667c4fa6c588edda66c90
jnamovic <jon.namovic@gmail.com>
2019-11-06 11:33:46 -0500

    juicer and cup rack fully implemented

---
+++ Assets/Scripts/Dispensible.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dispensible : MonoBehaviour
+{
+    private bool dispensed=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dispensed)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+        else {
+             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+
+    public void dispense() {
+        dispensed = true;
+    }
+}
---
+++ Assets/Scripts/Juicer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Juicer : MonoBehaviour
+{
+    public CircularDrive handle;
+    public GameObject spout;
+    public GameObject squisher;
+
+
+    private bool full;
+    private float juiceLeft;
+    private GameObject juiceableObject;
+    private GameObject juiceDrop;
+
+    private float SaveAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SaveAngle = handle.outAngle;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (handle.outAngle < SaveAngle && full) {
+            GameObject newDrop = Instantiate(juiceDrop);
+            newDrop.transform.position = spout.transform.position;
+            SaveAngle -= 90 / juiceableObject.GetComponent<Juiceable>().jucieUnits;
+            juiceLeft--;
+        }
+        
+        float scaleMove = ((handle.outAngle / -90) *.84f);
+        
+
+        squisher.transform.localScale = new Vector3(squisher.transform.localScale.x,.16f+scaleMove, squisher.transform.localScale.z);
+        
+        
+
+        
+        if (juiceLeft == 0) {
+            full = false;
+            if (juiceableObject != null) {
+                Destroy(juiceableObject);
+            }
+            
+
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Juiceable") {
+            if (juiceableObject!=null) {
+                Destroy(juiceableObject);
+            }
+            juiceableObject = other.gameObject;
+            LoadJuicer();
+
+        }
+    }
+
+
+    public void LoadJuicer(){
+        full = true;
+        juiceLeft = juiceableObject.GetComponent<Juiceable>().jucieUnits;
+        juiceDrop = juiceableObject.GetComponent<Juiceable>().juiceDrop;
+    }
+
+    public bool IsFull() {
+        return full;
+    }
+
+
+}
