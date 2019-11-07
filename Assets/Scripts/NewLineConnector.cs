d32caa09af561c28bb8c215f0999bbe2dac1e324
Joey Perrino <epiccrashv3@gmail.com>
2019-11-07 14:25:43 -0600

    Cable for Soda Jet created, not implemented yet

---
+++ Assets/Scripts/NewLineConnector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewLineConnector : MonoBehaviour
+{
+    private Transform other;
+    private LineRenderer line;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        other = GetComponent<SpringJoint>().connectedBody.transform;
+
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.SetPositions(new [] {transform.position, other.position });
+    }
+}
