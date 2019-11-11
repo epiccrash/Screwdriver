4e80c0e6da019578c104f236996af8b956830bf6
Joey Perrino <epiccrashv3@gmail.com>
2019-11-11 11:28:56 -0600

    Rotation on grab started

---
+++ Assets/Scripts/FixRotationOnGrab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRotationOnGrab : MonoBehaviour
+{
+    [SerializeField] float rotationTime = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.parent.name == "Left Hand" || transform.parent.name == "Right Hand")
+        {
+            //transform.rotation = Quaternion.Lerp(transform.rotation, transform.parent.rotation, rotationTime);
+            transform.LookAt(Vector3.forward);
+        }
+    }
+}
