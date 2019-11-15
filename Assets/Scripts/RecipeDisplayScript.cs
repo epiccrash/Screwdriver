8c7feadd80f2393d33cd146f388c9977ff760e84
Ilana Franklin <ilana.franklin@gmail.com>
2019-11-14 19:16:05 -0500

    Basic RecipeDisplayScript

--- Assets/Scripts/RecipeDisplayScript.cs
+++ Assets/Scripts/RecipeDisplayScript.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RecipeDisplayScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private TextMeshProUGUI _drinkNameDisplay;
+
+    [SerializeField]
+    private GameObject _ingredientRingParent;
+
+    private void Start()
     {
-        
+        _drinkNameDisplay.enabled = false;
+
+        DestroyIngredientRings();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DestroyIngredientRings()
     {
-        
+        foreach (Transform child in _ingredientRingParent.transform)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
