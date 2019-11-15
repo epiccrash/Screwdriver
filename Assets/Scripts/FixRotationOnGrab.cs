2c026643db8407944f9e3d1b759903c3c39e7632
Joey Perrino <epiccrashv3@gmail.com>
2019-11-14 20:35:49 -0500

    Working on rotation grab

--- Assets/Scripts/FixRotationOnGrab.cs
+++ Assets/Scripts/FixRotationOnGrab.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class FixRotationOnGrab : MonoBehaviour
 {
     [SerializeField] float rotationTime = 0.5f;
+    bool check;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        check = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.parent.name == "Left Hand" || transform.parent.name == "Right Hand")
+        if (transform.parent != null && (transform.parent.name == "LeftHand" || transform.parent.name == "RightHand"))
         {
             // transform.rotation = Quaternion.Lerp(transform.rotation, transform.parent.rotation, rotationTime * Time.deltaTime);
-            transform.LookAt(Vector3.forward);
+            //transform.LookAt(Vector3.forward);
+            Quaternion rotation = Quaternion.LookRotation(transform.parent.transform.right, Vector3.up);
+            // transform.rotation = rotation;
+            // transform.LookAt(transform.parent);
+            // transform.Rotate(new Vector3(0, 90, 0));
+            check = true;
+        }
+        else {
+            check = false;
         }
     }
 }
